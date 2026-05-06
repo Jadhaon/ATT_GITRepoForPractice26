6ff14540d3d66ab6cdd28e9149fd0cf21c39fca8
NamrataJadhao <namratajadhao0103@gmail.com>
2026-05-06 19:38:09 +0530

    adding pages and tests package

---
+++ src/pages/HomePage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class HomePage {
+
+	public void pageValidation()
+	{
+		System.out.println("Home page should be displayed");
+	}
+}
---
+++ src/tests/Hometest.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class Hometest {
+
+	public void validatedate()
+	{
+		System.out.println("Data validation");
+		System.out.println("User validation");
+	}
+	
+	
+}
