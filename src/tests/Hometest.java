997c5fcf734658776ca78753b63d8a210b4d367f
NamrataJadhao <namratajadhao0103@gmail.com>
2026-05-07 09:41:28 +0530

    validating text

--- src/tests/Hometest.java
+++ src/tests/Hometest.java
@@ -8,5 +8,18 @@ public class Hometest {
 		System.out.println("User validation");
 	}
 	
-	
+	public void verifyLogo()
+	{
+		System.out.println("Logo of the App is displayed");
+	}
+	public void verifyLoginfunctionality()
+	{
+		System.out.println("login of the App is validate");
+	}
+	public void validatetext()
+	{
+		System.out.println("Text validation");
+		System.out.println("Textdata validation");
+	}
 }
+
