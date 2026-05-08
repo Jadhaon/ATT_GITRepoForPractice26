cc402dce246597b9c79613aa15ad429ceaf59199
NamrataJadhao <namratajadhao0103@gmail.com>
2026-05-08 07:22:05 +0530

    adding profile test

---
+++ src/tests/Profiletest.java
@@ -0,0 +1,25 @@
+package tests;
+
+public class Hometest {
+
+	public void validatedate()
+	{
+		System.out.println("Data validation");
+		System.out.println("User validation");
+	}
+	
+	public void verifyLogo()
+	{
+		System.out.println("Logo of the App is displayed");
+	}
+	public void verifyLoginfunctionality()
+	{
+		System.out.println("login of the App is validate");
+	}
+	public void validatetext()
+	{
+		System.out.println("Text validation");
+		System.out.println("Textdata validation");
+	}
+}
+
