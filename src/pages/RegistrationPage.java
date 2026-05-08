a7ad1cb3b70d228535a0606ec067006763347d62
NamrataJadhao <namratajadhao0103@gmail.com>
2026-05-08 06:31:17 +0530

    adding RegistationPage

---
+++ src/pages/RegistrationPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class RegistrationPage {
+
+	public void m1()
+	{
+		System.out.println("Registration Page: m1");
+	}
+}
---
+++ src/tests/Registrationtest.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class Registrationtest {
+
+	public void m2()
+	{
+		System.out.println("Registration test : =========>");
+	}
+}
